169417828988ba31c3b1f5a24ca6e38187355abe
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-01-08 16:15:43 +0300

    Fixed and upgraded bet page

--- src/main/webapp/static/js/set-team-on-bet.js
+++
@@ -1,9 +0,0 @@
-function betOnFirst() {
-    let team = document.getElementById("on");
-    team.value = "FIRST";
-}
-
-function betOnSecond() {
-    let team = document.getElementById("on");
-    team.value = "SECOND";
-}
